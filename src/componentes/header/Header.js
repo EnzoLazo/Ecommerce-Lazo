c9c91ffd74ba0c865b7361eb86a94c3c6fa26e31
Enzo Lazo <enzojlazo@gmail.com>
2021-12-03 17:55:11 -0300

    agregando navbar

---
+++ src/componentes/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./header.css";
+import { NavBar } from './NavBar';
+
+
+export default function Header() {
+    return (
+        <div className="barra">
+            <NavBar/>
+        </div>
+    )
+}
+
---
+++ src/componentes/header/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import './navBar.css'
+import carrito from '../../assets/img/carrito.jpg';
+
+export const NavBar = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                <img src={carrito} alt="carrito" height="40px" />
+                    <Navbar.Brand href="#home">Ecommerce</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link   href="#home">Productos</Nav.Link>
+                            <Nav.Link   href="#link">Ofertas</Nav.Link>
+                            <Nav.Link   href="#link">Temporadas</Nav.Link>
+                            <Nav.Link   href="#link">Envios</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
